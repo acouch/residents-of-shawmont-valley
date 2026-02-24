a380faf536907e8adda2c8d020853aea50766a49
acouch <acouch@users.noreply.github.com>
2026-02-24 01:30:47 +0000

    jekyll build from Action 411c58578a868d4b263bfdcc21e24ff981a93d32

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,202 @@
+var store = [{
+        "title": "August 2023 Joint Meeting with URCA",
+        "excerpt":"This meeting was a joint meeting with Upper Roxborough Civic Assocation to review the SCEE Master plan.  ","categories": [],
+        "tags": [],
+        "url": "/events/230807-meeting/",
+        "teaser": null
+      },{
+        "title": "September 2023 Meeting",
+        "excerpt":"Agenda This will be a general neighborhood check-in. Captain James Kimrey from the 5th District will be attending to discuss recent thefts and other issues in the neighborhood There will be updates for the Shawmont Station Restoration, the Manayunk Canal Improvement Project, and the Wissahickon Transportation Center. We will review...","categories": [],
+        "tags": [],
+        "url": "/events/230926-meeting/",
+        "teaser": null
+      },{
+        "title": "November Meeting and Board Elections",
+        "excerpt":"This meeting will be held to elect a new slate of officers for RSVA. The current leadership team has put together a new slate of officers: Aaron Couch for President Pat Fiorella for Vice President Jennifer Diaz for Secretary Bob Small for Treasurer The nominating process is not closed, and...","categories": [],
+        "tags": [],
+        "url": "/events/231114-meeting/",
+        "teaser": null
+      },{
+        "title": "Winter Street Cleanup",
+        "excerpt":"The first cleanup of 2024 will take place on Saturday January 20th at 9am. We will gather at the on Shawmont Ave just past the where Shawmont Ave meets Minerva Street. Coffee and donuts will be served. The next clean up will be in April. NOTE: This event was originally...","categories": [],
+        "tags": [],
+        "url": "/events/240120-cleanup/",
+        "teaser": null
+      },{
+        "title": "Umbria Transfer Station Community Meeting",
+        "excerpt":"There will be a community meeting on January 31st at 7pm at the SCEE Meeting Center to discuss the new Transfer Station on Umbria Street.   Sanitation Department officials will be there to explain the project, answer questions, and hear community feedback.     ","categories": [],
+        "tags": [],
+        "url": "/events/240131-meeting/",
+        "teaser": null
+      },{
+        "title": "Umbria Transfer Station Follow-up Community Meeting",
+        "excerpt":"There will be a community meeting on March 13th at 7pm at the SCEE Meeting Center to discuss the new Transfer Station on Umbria Street. Sanitation Department officials will be there to explain the project, answer questions, and hear community feedback. This is the follow-up to an earlier community meeting...","categories": [],
+        "tags": [],
+        "url": "/events/240313-meeting/",
+        "teaser": null
+      },{
+        "title": "Spring Street Cleanup",
+        "excerpt":"The first cleanup of 2024 will take place on Saturday April 6th at 9am. We will gather at the on Shawmont Ave just past the where Shawmont Ave meets Minerva Street.   Coffee and donuts will be served. The next clean up will be in the summer.     ","categories": [],
+        "tags": [],
+        "url": "/events/240406-cleanup/",
+        "teaser": null
+      },{
+        "title": "RSVA Holiday Meeting and Social Hour",
+        "excerpt":"Join YOUR Residents of Shawmont Valley Association&nbsp; (RSVA) For an end of year Membership Meeting and Social Hour hosted by our neighbors, The Schuylkill Center for Environmental Education Join us from 6:30-8:00pm on December 11, 2024 - Join or renew your yearly membership with RSVA Membership dues are $10 for...","categories": [],
+        "tags": [],
+        "url": "/events/241211-holiday-meeting/",
+        "teaser": null
+      },{
+        "title": "Spring Street Cleanup",
+        "excerpt":"The first cleanup of 2025 will take place on Saturday April 5th at 9am. We will gather at the on Shawmont Ave just past the where Shawmont Ave meets Minerva Street.   Coffee and donuts will be served.         Photo from last Spring's cleanup.   ","categories": [],
+        "tags": [],
+        "url": "/events/250405-cleanup/",
+        "teaser": null
+      },{
+        "title": "Community Forum with Congressman Dwight Evans",
+        "excerpt":"Co-hosted by Residents of Shawmont Valley. If you can join us please rsvp at reptarik@pahouse.net. Here are more details: Wednesday April 23 from 5:30-7:30 p.m. 📍 Schuylkill Environmental Center Auditorium 8480 Hagys Mill Road, Philadelphia 5:30 p.m. – Doors Open, State &amp; Federal Service Assistance 6:00 p.m. – Program Begins...","categories": [],
+        "tags": [],
+        "url": "/events/250423-evans-community-mtg/",
+        "teaser": null
+      },{
+        "title": "June Board Elections meeting",
+        "excerpt":"Board election and general business meeting.  ","categories": [],
+        "tags": [],
+        "url": "/events/250625-meeting/",
+        "teaser": null
+      },{
+        "title": "Discussion of the Port Royal Gully Improvement Project",
+        "excerpt":"On January 15, 2026 at 6pm, RSVA will host a meeting with SCEE to discuss the Gully improvement project: January 15, 2026 at 6pm Schuylkill Center for Environmental Education This is a message from SCEE about the project: We are thrilled to announce that in February 2026 we will start...","categories": [],
+        "tags": [],
+        "url": "/events/260115-meeting/",
+        "teaser": null
+      },{
+        "title": "Winter meeting",
+        "excerpt":"On January 15, 2026 at 6pm, RSVA will hosted a meeting with SCEE to discuss the Gully improvement project. This is a message from SCEE about the project: We are thrilled to announce that in February 2026 we will start work on a project to reduce erosion and flooding on...","categories": [],
+        "tags": [],
+        "url": "/events/260226-meeting/",
+        "teaser": null
+      },{
+        "title": "Minerva St and Shawmont Ave",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/locations/minervashawmont/",
+        "teaser": null
+      },{
+        "title": "Nixon St and Shawmont Ave",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/locations/nixonshawmont/",
+        "teaser": null
+      },{
+        "title": "Roxborough Church",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/locations/roxboroughchurch/",
+        "teaser": null
+      },{
+        "title": "SCEE Meeting Center",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/locations/scee/",
+        "teaser": null
+      },{
+        "title": "Newsletter: New Transfer Station on Umbria, Flood Warning, ATV Meeting, and Other RSVA News",
+        "excerpt":"      ","categories": [],
+        "tags": [],
+        "url": "/news/240107-newsletter/",
+        "teaser": null
+      },{
+        "title": "Transfer Station Meeting Mar 13 & April 6 Cleanup",
+        "excerpt":"      ","categories": [],
+        "tags": [],
+        "url": "/news/240308-newsletter/",
+        "teaser": null
+      },{
+        "title": "Back to School Supplies from the 5th Police District",
+        "excerpt":"   ","categories": [],
+        "tags": [],
+        "url": "/news/230820-book-supplies/",
+        "teaser": null
+      },{
+        "title": "Newsletter: January 20th Cleanup and Other RSVA News",
+        "excerpt":"     ","categories": [],
+        "tags": [],
+        "url": "/news/231230-newsletter/",
+        "teaser": null
+      },,{
+    "title": "August 2023 Joint Meeting with URCA",
+    "excerpt":"This meeting was a joint meeting with Upper Roxborough Civic Assocation to review the SCEE Master plan.  ","url": "/_events/230807-meeting/"
+  },{
+    "title": "September 2023 Meeting",
+    "excerpt":"Agenda This will be a general neighborhood check-in. Captain James Kimrey from the 5th District will be attending to discuss recent thefts and other issues in the neighborhood There will be updates for the Shawmont Station Restoration, the Manayunk Canal Improvement Project, and the Wissahickon Transportation Center. We will review...","url": "/_events/230926-meeting/"
+  },{
+    "title": "November Meeting and Board Elections",
+    "excerpt":"This meeting will be held to elect a new slate of officers for RSVA. The current leadership team has put together a new slate of officers: Aaron Couch for President Pat Fiorella for Vice President Jennifer Diaz for Secretary Bob Small for Treasurer The nominating process is not closed, and...","url": "/_events/231114-meeting/"
+  },{
+    "title": "Winter Street Cleanup",
+    "excerpt":"The first cleanup of 2024 will take place on Saturday January 20th at 9am. We will gather at the on Shawmont Ave just past the where Shawmont Ave meets Minerva Street. Coffee and donuts will be served. The next clean up will be in April. NOTE: This event was originally...","url": "/_events/240120-cleanup/"
+  },{
+    "title": "Umbria Transfer Station Community Meeting",
+    "excerpt":"There will be a community meeting on January 31st at 7pm at the SCEE Meeting Center to discuss the new Transfer Station on Umbria Street.   Sanitation Department officials will be there to explain the project, answer questions, and hear community feedback.     ","url": "/_events/240131-meeting/"
+  },{
+    "title": "Umbria Transfer Station Follow-up Community Meeting",
+    "excerpt":"There will be a community meeting on March 13th at 7pm at the SCEE Meeting Center to discuss the new Transfer Station on Umbria Street. Sanitation Department officials will be there to explain the project, answer questions, and hear community feedback. This is the follow-up to an earlier community meeting...","url": "/_events/240313-meeting/"
+  },{
+    "title": "Spring Street Cleanup",
+    "excerpt":"The first cleanup of 2024 will take place on Saturday April 6th at 9am. We will gather at the on Shawmont Ave just past the where Shawmont Ave meets Minerva Street.   Coffee and donuts will be served. The next clean up will be in the summer.     ","url": "/_events/240406-cleanup/"
+  },{
+    "title": "RSVA Holiday Meeting and Social Hour",
+    "excerpt":"Join YOUR Residents of Shawmont Valley Association&nbsp; (RSVA) For an end of year Membership Meeting and Social Hour hosted by our neighbors, The Schuylkill Center for Environmental Education Join us from 6:30-8:00pm on December 11, 2024 - Join or renew your yearly membership with RSVA Membership dues are $10 for...","url": "/_events/241211-holiday-meeting/"
+  },{
+    "title": "Spring Street Cleanup",
+    "excerpt":"The first cleanup of 2025 will take place on Saturday April 5th at 9am. We will gather at the on Shawmont Ave just past the where Shawmont Ave meets Minerva Street.   Coffee and donuts will be served.         Photo from last Spring's cleanup.   ","url": "/_events/250405-cleanup/"
+  },{
+    "title": "Community Forum with Congressman Dwight Evans",
+    "excerpt":"Co-hosted by Residents of Shawmont Valley. If you can join us please rsvp at reptarik@pahouse.net. Here are more details: Wednesday April 23 from 5:30-7:30 p.m. 📍 Schuylkill Environmental Center Auditorium 8480 Hagys Mill Road, Philadelphia 5:30 p.m. – Doors Open, State &amp; Federal Service Assistance 6:00 p.m. – Program Begins...","url": "/_events/250423-evans-community-mtg/"
+  },{
+    "title": "June Board Elections meeting",
+    "excerpt":"Board election and general business meeting.  ","url": "/_events/250625-meeting/"
+  },{
+    "title": "Discussion of the Port Royal Gully Improvement Project",
+    "excerpt":"On January 15, 2026 at 6pm, RSVA will host a meeting with SCEE to discuss the Gully improvement project: January 15, 2026 at 6pm Schuylkill Center for Environmental Education This is a message from SCEE about the project: We are thrilled to announce that in February 2026 we will start...","url": "/_events/260115-meeting/"
+  },{
+    "title": "Winter meeting",
+    "excerpt":"On January 15, 2026 at 6pm, RSVA will hosted a meeting with SCEE to discuss the Gully improvement project. This is a message from SCEE about the project: We are thrilled to announce that in February 2026 we will start work on a project to reduce erosion and flooding on...","url": "/_events/260226-meeting/"
+  },{
+    "title": "About the RSVA",
+    "excerpt":"Residents of Shawmont Valley is neighborhood association established by concerned neighbors in 1980 located in the upper Roxborough neighborhood of Philadelphia. RSVA is a Registered Community Organization (RCO). With a focus on the preservation of greenways, waterways, and the overall watershed area, and of course, issues that impact our lives...","url": "/about/"
+  },{
+    "title": "Meetings and Events",
+    "excerpt":"Residents of Shawmont Valley Association meets on a semi-regular basis, and as needed when zoning or other matters arise. Upcoming Meetings Winter meeting February 26, 2026 On January 15, 2026 at 6pm, RSVA will hosted a meeting with SCEE to discuss the Gully improvement project.This is a message from SCEE...","url": "/events/"
+  },{
+    "title": "Residents of Shawmont Valley Association",
+    "excerpt":"Meetings RSVA meetings and events. List of Meetings Map View the catchment area for RSVA and local civics. RSVA Map Join Join the RSVA to attend meetings and vote on outcomes. Join the RSVA Upcoming Meetings and Events Winter meeting February 26, 2026 On January 15, 2026 at 6pm, RSVA...","url": "/"
+  },{
+    "title": "Join",
+    "excerpt":"Membership dues are $10 for individuals and $15 for a two-person household. Dues are payable in cash or check to Bob Small (21 River Road) made out to RSVA. Sign up for updates Fill out the form below to receive email updates about meetings and events. This is a low-volume...","url": "/join/"
+  },{
+    "title": null,
+    "excerpt":"var idx = lunr(function () { this.field('title') this.field('excerpt') this.field('categories') this.field('tags') this.ref('id') this.pipeline.remove(lunr.trimmer) for (var item in store) { this.add({ title: store[item].title, excerpt: store[item].excerpt, categories: store[item].categories, tags: store[item].tags, id: item }) } }); $(document).ready(function() { $('input#search').on('keyup', function () { var resultdiv = $('#results'); var query = $(this).val().toLowerCase(); var result = idx.query(function...","url": "/assets/js/lunr/lunr-en.js"
+  },{
+    "title": null,
+    "excerpt":"step1list = new Array(); step1list[\"ΦΑΓΙΑ\"] = \"ΦΑ\"; step1list[\"ΦΑΓΙΟΥ\"] = \"ΦΑ\"; step1list[\"ΦΑΓΙΩΝ\"] = \"ΦΑ\"; step1list[\"ΣΚΑΓΙΑ\"] = \"ΣΚΑ\"; step1list[\"ΣΚΑΓΙΟΥ\"] = \"ΣΚΑ\"; step1list[\"ΣΚΑΓΙΩΝ\"] = \"ΣΚΑ\"; step1list[\"ΟΛΟΓΙΟΥ\"] = \"ΟΛΟ\"; step1list[\"ΟΛΟΓΙΑ\"] = \"ΟΛΟ\"; step1list[\"ΟΛΟΓΙΩΝ\"] = \"ΟΛΟ\"; step1list[\"ΣΟΓΙΟΥ\"] = \"ΣΟ\"; step1list[\"ΣΟΓΙΑ\"] = \"ΣΟ\"; step1list[\"ΣΟΓΙΩΝ\"] = \"ΣΟ\"; step1list[\"ΤΑΤΟΓΙΑ\"] = \"ΤΑΤΟ\"; step1list[\"ΤΑΤΟΓΙΟΥ\"] = \"ΤΑΤΟ\"; step1list[\"ΤΑΤΟΓΙΩΝ\"] = \"ΤΑΤΟ\"; step1list[\"ΚΡΕΑΣ\"]...","url": "/assets/js/lunr/lunr-gr.js"
+  },{
+    "title": null,
+    "excerpt":"var store = [ {%- for c in site.collections -%} {%- if forloop.last -%} {%- assign l = true -%} {%- endif -%} {%- assign docs = c.docs | where_exp:'doc','doc.search != false' -%} {%- for doc in docs -%} {%- if doc.header.teaser -%} {%- capture teaser -%}{{ doc.header.teaser }}{%- endcapture...","url": "/assets/js/lunr/lunr-store.js"
+  },{
+    "title": "Map of the RSVA",
+    "excerpt":"","url": "/map/"
+  },{
+    "title": "News",
+    "excerpt":"These are neighborhood related items shared with RSVA. To share something with our members, contact us.  Recent Community News   {% assign entries_layout = page.entries_layout | default: 'list' %}    {% include documents-collection.html collection=page.collection sort_order=reverse type=entries_layout %}  ","url": "/news/"
+  },{
+    "title": null,
+    "excerpt":"{% if page.xsl %} {% endif %} {% assign collections = site.collections | where_exp:'collection','collection.output != false' %}{% for collection in collections %}{% assign docs = collection.docs | where_exp:'doc','doc.sitemap != false' %}{% for doc in docs %} {{ doc.url | replace:'/index.html','/' | absolute_url | xml_escape }} {% if doc.last_modified_at or doc.date...","url": "/sitemap.xml"
+  },{
+    "title": null,
+    "excerpt":"Sitemap: {{ \"sitemap.xml\" | absolute_url }} ","url": "/robots.txt"
+  },{
+    "title": null,
+    "excerpt":"{% if page.xsl %}{% endif %}Jekyll{{ site.time | date_to_xmlschema }}{{ page.url | absolute_url | xml_escape }}{% assign title = site.title | default: site.name %}{% if page.collection != \"posts\" %}{% assign collection = page.collection | capitalize %}{% assign title = title | append: \" | \" | append: collection %}{% endif...","url": "/feed.xml"
+  }]
